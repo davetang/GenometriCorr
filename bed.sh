5386b7e901f47ee8cdfe186c6250d67ad28e1f78
Dave Tang <davetingpongtang@gmail.com>
2014-11-01 23:48:15 +0900

    Example with equal distance

--- bed.sh
+++ bed.sh
@@ -5,3 +5,4 @@ mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -e \
 
 randomBed -seed 23 -l 1000 -n 50000 -g hg19.genome | sortBed > a.bed
 randomBed -seed 31 -l 1000 -n 50000 -g hg19.genome | sortBed > b.bed
+cat a.bed | awk '{OFS="\t";print $1, $2+2500, $3+2500, $4, $5, $6}' > c.bed
