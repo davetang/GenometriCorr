e933fb1be2f4ae9d0e92bf4284e4a06ae10e27d4
Dave Tang <davetingpongtang@gmail.com>
2015-02-02 09:06:33 +0900

    Run analysis on all chr

--- script/run_analysis.pl
+++ script/run_analysis.pl
@@ -30,6 +30,11 @@ names(human.chrom.length) <- c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6",
                                "chr13", "chr14", "chr15", "chr16", "chr17", "chr18",
                                "chr19", "chr20", "chr21", "chr22", "chrY", "chrX")
 
+my_chr <- c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6",
+            "chr7", "chr8", "chr9", "chr10", "chr11", "chr12",
+            "chr13", "chr14", "chr15", "chr16", "chr17", "chr18",
+            "chr19", "chr20", "chr21", "chr22", "chrY", "chrX")
+
 pn.area <- 100
 pn.dist <- 100
 pn.jacc <- 100
@@ -38,7 +43,8 @@ a <- as(import('$a'),"RangedData")
 b <- as(import('$b'), "RangedData")
 
 a_vs_b <- GenometriCorrelation(a, b, chromosomes.length = human.chrom.length,
-                               chromosomes.to.proceed = c("chr1", "chr2", "chr3"),
+                               #chromosomes.to.proceed = c("chr1", "chr2", "chr3"),
+                               chromosomes.to.proceed = my_chr,
                                ecdf.area.permut.number = pn.area,
                                mean.distance.permut.number = pn.dist,
                                jaccard.measure.permut.number = pn.jacc,
