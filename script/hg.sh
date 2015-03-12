9a8830219f7b5f3b522c72835193147c4369de28
Dave Tang <davetingpongtang@gmail.com>
2015-03-12 13:38:53 +0900

    hg38

--- script/hg.sh
+++ script/hg.sh
@@ -2,3 +2,6 @@
 
 #filter out unassembled chromosomes, random chromosomes, and the mitochrondrial chromosome
 mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -e "select chrom, size from hg19.chromInfo" | grep -v "_" | grep -v "chrM" > hg19.genome
+
+#for hg38
+mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -e "select chrom, size from hg38.chromInfo" | grep -v "_" | grep -v "chrM" > hg38.genome
